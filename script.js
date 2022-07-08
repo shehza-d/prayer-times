01d22b005667c523230c0e0d76e178b0a3b0cf82
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-07-09 02:42:07 +0500

    done with api response

--- script.js
+++ script.js
@@ -18,11 +18,12 @@ let myFunction = () => {
 
 
 
-
-// http://api.aladhan.com/v1/calendarByAddress?address=Sultanahmet%20Mosque,%20Istanbul,%20Turkey&method=2&month=04&year=2017
-
+//maarig sir api       https://api.aladhan.com/v1/timingsByCity?city=${city}&country=""&method=8
+//zaid 617   api       https://api.aladhan.com/v1/timingsByCity?city=${city}&country=${country}&method=1
+//                     http://api.aladhan.com/v1/calendarByAddress?address=Sultanahmet%20Mosque,%20Istanbul,%20Turkey&method=2&month=04&year=2017
+// my recommendation   https://api.aladhan.com/v1/timings/1398332113?latitude=51.508515&longitude=-0.1254872&method=1
     // let cityy = document.querySelector("#city").value;
-    axios.get(`https://api.aladhan.com/v1/timings/1398332113?latitude=51.508515&longitude=-0.1254872&method=1`)
+    axios.get(`https://api.aladhan.com/v1/timingsByCity?city=${city}&country=""&method=1`)
         .then(function (response) {
             // handle success
             const data = response.data;
@@ -35,15 +36,8 @@ let myFunction = () => {
 
 
             // document.querySelector("#tempC").innerText = data.current.temp_c + "°C";
-           
-
-
-
-
         })
 
 
 }
 // getWeatherData();
-
-    // 25175e31b7074cfc895204529222906 key
