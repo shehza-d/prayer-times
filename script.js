6f581c639b074c78833693bc4fa86dc413c5b458
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-07-09 23:34:32 +0500

    latest

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //getting date from user
+let city;
 const d = new Date();
 const day = d.getDate();
 const month = d.getMonth() + 1;
@@ -48,9 +49,11 @@ let myFunction = () => {
         }
 
 
-        let city = document.getElementById("city_input").value;
+        city = document.getElementById("city_input").value;
+        manual_input(city)
         console.log(city);
         // document.querySelector("#city_name").innerText = city;
+
         // axios.get(`https://api.aladhan.com/v1/timingsByCity?city=${city}&country=""&method=1`)
         //     .then(function (response) {
         //         // handle success
@@ -58,11 +61,9 @@ let myFunction = () => {
         //         console.log(data);
 
         //         // document.querySelector("#date").innerText = data.data.date.gregorian.date;
-
         //         document.querySelector("#day").innerText = data.data.date.gregorian.weekday.en;
         //         // document.querySelector("#date").innerHTML = data.data.date.hijri.date;
         //         // document.querySelector("#date").innerHTML = data.data.date.hijri.day;
-
 
         //         document.querySelector("#namaz1").innerText = `Fajr : ${data.data.timings.Fajr}`
         //         document.querySelector("#namaz2").innerText = `Dhuhr : ${data.data.timings.Dhuhr}`;
@@ -87,47 +88,28 @@ let myFunction = () => {
     }
 }
 
-// let myFunction = () => {
+// let manual_input = () => {
+ // axios.get(`https://api.aladhan.com/v1/timingsByCity?city=${city}&country=""&method=1`)
+        //     .then(function (response) {
+        //         // handle success
+        //         const data = response.data;
+        //         console.log(data);
 
+        //         // document.querySelector("#date").innerText = data.data.date.gregorian.date;
+        //         document.querySelector("#day").innerText = data.data.date.gregorian.weekday.en;
+        //         // document.querySelector("#date").innerHTML = data.data.date.hijri.date;
+        //         // document.querySelector("#date").innerHTML = data.data.date.hijri.day;
 
-    // let city = document.getElementById("city").value;
-    // document.querySelector("#city_name").innerText = city;
-    // axios.get(`https://api.aladhan.com/v1/timingsByCity?city=${city}&country=""&method=1`)
-    //     .then(function (response) {
-    //         // handle success
-    //         const data = response.data;
-    //         console.log(data);
-
-    //         // document.querySelector("#date").innerText = data.data.date.gregorian.date;
-    //         document.querySelector("#day").innerText = data.data.date.gregorian.weekday.en;
-
-    //         document.querySelector("#namaz1").innerText = `Fajr : ${data.data.timings.Fajr}`
-    //         document.querySelector("#namaz2").innerText = `Dhuhr : ${data.data.timings.Dhuhr}`;
-    //         document.querySelector("#namaz3").innerText = `Asr : ${data.data.timings.Asr}`;
-    //         document.querySelector("#namaz4").innerText = `Maghrib : ${data.data.timings.Maghrib}`;
-    //         document.querySelector("#namaz5").innerText = `Isha : ${data.data.timings.Isha}`;
-    //     })
-    // }
-
-
-
-
-
-
-
-
-
-
-
-
-
+        //         document.querySelector("#namaz1").innerText = `Fajr : ${data.data.timings.Fajr}`
+        //         document.querySelector("#namaz2").innerText = `Dhuhr : ${data.data.timings.Dhuhr}`;
+        //         document.querySelector("#namaz3").innerText = `Asr : ${data.data.timings.Asr}`;
+        //         document.querySelector("#namaz4").innerText = `Maghrib : ${data.data.timings.Maghrib}`;
+        //         document.querySelector("#namaz5").innerText = `Isha : ${data.data.timings.Isha}`;
+        //     })
+// }
 
     //maarig sir api       https://api.aladhan.com/v1/timingsByCity?city=${city}&country=""&method=8
     //zaid 617   api       https://api.aladhan.com/v1/timingsByCity?city=${city}&country=${country}&method=1
     //                     http://api.aladhan.com/v1/calendarByAddress?address=Sultanahmet%20Mosque,%20Istanbul,%20Turkey&method=2&month=04&year=2017
     // my recommendation   https://api.aladhan.com/v1/timings/1398332113?latitude=51.508515&longitude=-0.1254872&method=1
     // let cityy = document.querySelector("#city").value;
-
-
-
-// }
