6f7b337fbbb008b34d5d504f41b0db499929a0ec
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-07-09 13:59:48 +0500

    getting namaz time with auto location done

--- script.js
+++ script.js
@@ -1,43 +1,74 @@
+let latitude;
+let longitude;
+// getting user location
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+} else {
+    alert("Location Access deined or Geolocation is not supported by this browser.");
+}
+function showPosition(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    console.log("latitude: " + latitude);
+    console.log("longitude: " + longitude);
+}
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            alert("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            alert("An unknown error occurred.");
+            break;
+    }
+}
+
 let myFunction = () => {
 
-// getting user location
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//maarig sir api       https://api.aladhan.com/v1/timingsByCity?city=${city}&country=""&method=8
-//zaid 617   api       https://api.aladhan.com/v1/timingsByCity?city=${city}&country=${country}&method=1
-//                     http://api.aladhan.com/v1/calendarByAddress?address=Sultanahmet%20Mosque,%20Istanbul,%20Turkey&method=2&month=04&year=2017
-// my recommendation   https://api.aladhan.com/v1/timings/1398332113?latitude=51.508515&longitude=-0.1254872&method=1
-    // let cityy = document.querySelector("#city").value;
-    axios.get(`https://api.aladhan.com/v1/timingsByCity?city=${city}&country=""&method=1`)
+    if (latitude == undefined) {
+        let city = document.getElementById("city").value;
+        axios.get(`https://api.aladhan.com/v1/timingsByCity?city=${city}&country=""&method=1`)
+            .then(function (response) {
+                // handle success
+                const data = response.data;
+                console.log(data);
+                // document.querySelector("#tempC").innerText = data.current.temp_c + "°C";
+            })
+    } else {
+        axios.get(`https://api.aladhan.com/v1/timings/1398332113?latitude=${latitude}&longitude=${longitude}&method=1`)
         .then(function (response) {
             // handle success
             const data = response.data;
-
             console.log(data);
-
-           
-
-
-
-
             // document.querySelector("#tempC").innerText = data.current.temp_c + "°C";
         })
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //maarig sir api       https://api.aladhan.com/v1/timingsByCity?city=${city}&country=""&method=8
+    //zaid 617   api       https://api.aladhan.com/v1/timingsByCity?city=${city}&country=${country}&method=1
+    //                     http://api.aladhan.com/v1/calendarByAddress?address=Sultanahmet%20Mosque,%20Istanbul,%20Turkey&method=2&month=04&year=2017
+    // my recommendation   https://api.aladhan.com/v1/timings/1398332113?latitude=51.508515&longitude=-0.1254872&method=1
+    // let cityy = document.querySelector("#city").value;
+
 
 
 }
-// getWeatherData();
